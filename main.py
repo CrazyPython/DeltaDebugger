75ae1144ead8c9b1a5dd9440fc5823c24a125667
CrazyPython <Jamtlu@gmail.com>
2013-01-21 12:31:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-import os.path
+import os.path,random
+def fuzzer(maxlength=1024,t = str):
+  if t == str:
+    string_length = int(random.random() * maxlength)   
+    out = ""
+    for i in range(0, string_length):
+      out += chr(int(random.random() * 96 + 32)) 
+  elif t == int:
+    out = random.randomint(0,maxlength)
+  elif t == float:
+    out = random.random()
+  elif t == list:
+    #nesting not supported
+    #random list
+    #recursive
+    length = random.random * maxlength 
+    gentypes = [random.choice(str,int,float) for i in range(length)]
+    out = [fuzzer(maxlength,ty) for I,ty in zip(length,gentypes)]
+  return out
 def writetemp(what):
   if not os.path.exists("temp"):
     f = open(test)
@@ -15,15 +33,24 @@ def writetemp(what):
     sys.err.write("ERROR:Directory needs to not have temp or test so deltadebugger can write a temporary file")
     time.sleep(20)
     sys.exit()
-def pythontest(input):
-  url = writetemp(input)
-  try:
-    execfile(url)
-  except:
-    return "FAIL"
+def pythontest(cod,mode,fuzzertype):
+  url = writetemp(code)
+  if mode == "program"
+    try:
+        execfile(url)
+    except:
+        return "FAIL"
+    else:
+        return "PASS"
   else:
-    return "PASS"
-def ddmin(s,test):
+    try:
+        execfile(url,locals = [fuzzer(fuzzertype)])
+    except:
+        return "FAIL"
+    else:
+        return "PASS"
+def ddmin(s,tester=pythontest,mode = "program",fuzzertype=str): #mode is "program" or "function"(fuzzer)
+    test = lambda code:tester(code,mode,fuzzertype)
     if test(s) == "FAIL":
       print "Works"
     n = 2     # Initial granularity
